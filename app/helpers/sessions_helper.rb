d166fc7efc5a4b56337b1a0b4ab3e66bda9c108f
savitha-r <savitha.r@2359media.com>
2014-01-22 20:22:53 +0530

    fb auth added

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,8 @@ module SessionsHelper
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
   	end
 
+  	def signed_in?
+      !current_user.nil?
+    end
+
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, 'YOUR-APP-ID-HERE', 'YOUR-APP-SECRET-HERE'
+  provider :facebook, "209495965909628", "b3fa8b1c7c6c3ef2a6ce545be19169e8"
 end
