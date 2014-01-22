5d75bdae42a5597643599d22e485e4009b9900a1
savitha-r <savitha.r@2359media.com>
2014-01-22 19:10:41 +0530

    add user model and sessions controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AuctionHouse::Application.config.secret_key_base = '6f3a6f273d375b67e43ceaab077df0a81b49f237daa86cc6d810b4f032531d15ac6f589ffd748fe043ec2a04d74aa5d7cb1c1368d68371d94d85dd522a969f0c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AuctionHouse::Application.config.session_store :cookie_store, key: '_AuctionHouse_session'
